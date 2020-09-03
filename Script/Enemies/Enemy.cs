b605fbaea5deb02521c46267c20065a33b1220f2
forexms78 <49295076+forexms78@users.noreply.github.com>
2020-09-03 17:50:37 +0900

    에셋 파일 미완료

---
+++ Script/Enemies/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+        float health = 50.0f;
+
+    public float Health
+    {
+        get { return health; }
+    }
+
+    private void Start()
+    {
+        Debug.Log("health : " + health);
+    }
+
+    void TakeDamage(int value)
+    {
+        health -= value;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+        public float GetGealth()
+        {
+            return health;
+        }
+
+        void Die()
+        {
+            Destroy(this.gameObject);
+        }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        TakeDamage(10);
+        Debug.Log("health : " + health);
+        collision.gameObject.SetActive(false);
+    }
+
+}
---
+++ Script/Player/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void Start()
+    {
+        Invoke("DestroySelf", 2.0f);
+    }
+
+    void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
